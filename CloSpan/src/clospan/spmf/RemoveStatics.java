269b8cfa103b0eabb98172eccb9be8a0e93503c5
Michael Hundt <michael.hundt@uni-konstanz.de>
2015-06-17 15:54:10 +0200

    CloSpan .. import hopefully works now

---
+++ CloSpan/src/clospan/spmf/RemoveStatics.java
@@ -0,0 +1,17 @@
+package clospan.spmf;
+
+
+
+
+/**
+ *
+ * @author antonio
+ */
+public class RemoveStatics {
+
+    public static void clear() {
+        ItemAbstractionPairCreator.sclear();
+        Abstraction_Qualitative.clear();
+        PatternCreator.sclear();
+    }
+}
